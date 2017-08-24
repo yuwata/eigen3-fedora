ce57c69027dec263cdd9851583b18fc947900799
Yu Watanabe <watanabe.yu+github@gmail.com>
2017-08-24 18:11:21 +0900

    update script

--- nightly-build.sh
+++ nightly-build.sh
@@ -11,10 +11,9 @@ REPO_DIR=${HOME}/git/${REPO_NAME}
 
 HASH_OLD=$(grep -e '^[%#]global commit' ${SOURCE_DIR}/${REPO_NAME}.spec | awk '{ print $3 }')
 if [[ -z $HASH_OLD ]]; then
-    echo 'error: cannot obtain commit hash.' >&2
+    echo 'error: cannot obtain old commit hash.' >&2
     exit 1
 fi
-HASH_OLD_SHORT=${HASH_OLD:0:7}
 
 if ! hg -R $REPO_DIR pull; then
     echo "error: 'hg pull' failed." >&2
@@ -25,34 +24,38 @@ if ! HASH_NEW=$(hg -R $REPO_DIR branches | grep -e "^${BRANCH}" | awk -F : '{ pr
     echo "error: 'hg branches' failed." >&2
     exit 12
 fi
+
+if [[ "$HASH_NEW" == "$HASH_OLD" ]]; then
+    echo 'info: not necessary to update spec file.' >&2
+    exit
+fi
+
+HASH_OLD_SHORT=${HASH_OLD:0:7}
 HASH_NEW_SHORT=${HASH_NEW:0:7}
 
-if [[ "$HASH_NEW" != "$HASH_OLD" ]]; then
+VERSION=$(grep -e '^Version:' ${SOURCE_DIR}/${REPO_NAME}.spec | awk '{ print $2 }')
 
-    VERSION=$(grep -e '^Version:' ${SOURCE_DIR}/${REPO_NAME}.spec | awk '{ print $2 }')
-
-    RELEASE_OLD=$(grep -e '^Release:' ${SOURCE_DIR}/${REPO_NAME}.spec | sed -e 's/^Release:[[:space:]]*//; s/%.*$//;')
-    RELEASE_MAIN=$(echo $RELEASE_OLD | sed -e 's/\.[[:digit:]]*//')
-    RELEASE_SUB_OLD=$(echo $RELEASE_OLD | sed -e 's/[[:digit:]]*\.*//')
-    if [[ -z $RELEASE_SUB_OLD ]]; then
-        RELEASE_SUB_OLD=0
-    fi
-
-    RELEASE_SUB_NEW=$(( $RELEASE_SUB_OLD + 1 ))
-    RELEASE_NEW=${RELEASE_MAIN}.${RELEASE_SUB_NEW}
-
-    WEEKDAY=$(date "+%a")
-    MONTH=$(date "+%b")
-    DAY=$(date "+%d")
-    YEAR=$(date "+%Y")
-
-    sed -e '/^[%#]global commit/ { s/^#/%/; s/'${HASH_OLD}'/'${HASH_NEW}'/ }' \
-        -e '/^Release:/ s/'${RELEASE_OLD}'/'${RELEASE_NEW}'/' \
-        -e "/^%changelog/ a\
-* ${WEEKDAY} ${MONTH} ${DAY} ${YEAR} Yu Watanabe <watanabe.yu@gmail.com> - ${VERSION}-${RELEASE_NEW}.hg${HASH_NEW_SHORT}\\
-- Update to latest snapshot ${HASH_NEW}\\
-" \
-        -i ${SOURCE_DIR}/${REPO_NAME}.spec
-
-    git -C $SOURCE_DIR commit -a -m 'Update to latest snapshot'
+RELEASE_OLD=$(grep -e '^Release:' ${SOURCE_DIR}/${REPO_NAME}.spec | sed -e 's/^Release:[[:space:]]*//; s/%.*$//;')
+RELEASE_MAIN=$(echo $RELEASE_OLD | sed -e 's/\.[[:digit:]]*//')
+RELEASE_SUB_OLD=$(echo $RELEASE_OLD | sed -e 's/[[:digit:]]*\.*//')
+if [[ -z $RELEASE_SUB_OLD ]]; then
+    RELEASE_SUB_OLD=0
 fi
+RELEASE_SUB_NEW=$(( $RELEASE_SUB_OLD + 1 ))
+RELEASE_NEW=${RELEASE_MAIN}.${RELEASE_SUB_NEW}
+
+WEEKDAY=$(date "+%a")
+MONTH=$(date "+%b")
+DAY=$(date "+%d")
+YEAR=$(date "+%Y")
+
+CHANGE_LOG="Update to latest git snapshot ${HASH_NEW}\n"
+
+sed -e '/^[%#]global commit/ { s/^#/%/; s/'${HASH_OLD}'/'${HASH_NEW}'/ }' \
+    -e '/^Release:/ s/'${RELEASE_OLD}'/'${RELEASE_NEW}'/' \
+    -e "/^%changelog/ a\
+* ${WEEKDAY} ${MONTH} ${DAY} ${YEAR} Yu Watanabe <watanabe.yu@gmail.com> - ${VERSION}-${RELEASE_NEW}.hg${HASH_NEW_SHORT}\\
+- ${CHANGE_LOG}" \
+    -i ${SOURCE_DIR}/${REPO_NAME}.spec
+
+git -C $SOURCE_DIR commit -a -m "$(echo -e ${CHANGE_LOG})"
